a32d9c9fdc41a005773999852e60e870753a7e5a
Ektaroy <97738792+Ektaroy@users.noreply.github.com>
2023-01-06 23:51:00 +0530

    updated

--- LL/LL9.cpp
+++ LL/LL9.cpp
@@ -1,32 +1,104 @@
-void kReverse(int k) {
-    linkedlist* prev=NULL;
-    while(this->size>0){
-        linkedlist* curr= new linkedlist();
-        if(this->size>=k){
-        for(int i=0;i<k;i++){
-            int val=this->getFirst();
-            this->removeFirst();
-            curr->addFirst(val);
-        }
-        }
-        else{
-            int s=this->size;
-            for(int i=0;i<s;i++){
-                int val=this->getFirst();
-            this->removeFirst();
-            curr->addLast(val);
-            }
-        }
-        if(prev==NULL){
-            prev=curr;
-        }
-        else{
-            prev->tail->next=curr->head;
-            prev->tail=curr->tail;
-            prev->size+=curr->size;
-        }
-    }
-    this->head=prev->head;
-    this->tail=prev->tail;
-    this->size=prev->size;
+/****************************************************************
+ 
+    Following is the class structure of the Node class:
+
+        class Node
+        {
+        public:
+	        int data;
+	        Node *next;
+	        Node(int data)
+	        {
+		        this->data = data;
+		        this->next = NULL;
+	        }
+        };
+
+*****************************************************************/
+
+Node *reversehelper(Node *head) {
+  // Write your code here
+  if (head == NULL || head->next == NULL)
+    return head;
+
+  Node *ans = reversehelper(head->next);
+  Node *temp = ans;
+  while (temp->next != NULL) {
+    temp = temp->next;
   }
+  temp->next = head;
+  head->next = NULL;
+  head = ans;
+  return head;
+}
+
+pair<Node *, Node *> reverse(Node *head) {
+	Node*h= reversehelper(head);
+	Node*t= h;
+
+if(h==NULL)
+{
+
+  pair<Node *, Node *> p;
+  p.first = h;
+  p.second = h;
+  return p;
+}
+
+while(t->next!=NULL)
+{
+t=t->next;
+}
+
+	pair<Node*,Node*> p;
+	p.first=h;
+	p.second=t;
+	return p;
+	 
+	  }
+
+int size(Node*head)
+{
+	int ans=0;
+	Node*temp=head;
+	while(temp!=NULL)
+	{
+		temp=temp->next;
+		ans++;
+	}
+	return ans;
+}
+Node *kReverse(Node *head, int k)
+{
+	//Write your code here
+
+if(head==NULL)
+return head;
+
+	if(size(head)<=k)
+	{
+          pair<Node *, Node *> p=reverse(head);
+        
+          return p.first;
+        }
+	
+
+	
+	Node*temp=head;
+
+Node*h1=head;
+Node*t1=head;
+
+	for(int i=0;i<k-1;i++ ){
+		t1=t1->next;
+	}
+
+Node*h2=t1->next;
+t1->next=NULL;
+        pair<Node *, Node *> p=reverse(h1);
+		h1=p.first;
+		t1=p.second;
+
+                t1->next = kReverse(h2,k);
+				return h1;
+}
